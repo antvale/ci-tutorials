443dbbfd4f863a1db9b026799b4f1a2b9766312a
antvale <ant.valentino@gmail.com>
2018-11-23 08:36:40 +0100

    initial commit

---
+++ mvn-artifactoryoss/my-newapp/src/main/java/com/mycompany/newapp/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.newapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static String getMyFriend(){
+      return "John";
+}
+}
